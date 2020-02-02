b64568612e287533079845afe6071f00769384de
Smokyn <smokyn_cosmin@yahoo.com>
2020-02-02 18:19:19 +0200

    Updates

---
+++ precache-manifest.73d0f0b6da99c33dbaa5b7f620e722a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b06a0ba6242cbfa18b5e588fc6d4da94",
+    "url": "/ITLogger/index.html"
+  },
+  {
+    "revision": "24cd540ac065633b7653",
+    "url": "/ITLogger/static/css/2.2623327d.chunk.css"
+  },
+  {
+    "revision": "24cd540ac065633b7653",
+    "url": "/ITLogger/static/js/2.785e298f.chunk.js"
+  },
+  {
+    "revision": "a683db86ff64ed77ad7f84825de6764c",
+    "url": "/ITLogger/static/js/2.785e298f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ffa9a01393880d6f426",
+    "url": "/ITLogger/static/js/main.5c31c255.chunk.js"
+  },
+  {
+    "revision": "f84641b048537003f464",
+    "url": "/ITLogger/static/js/runtime-main.d1597801.js"
+  }
+]);
